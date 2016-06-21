8b6795d1f14949edeb31d5fe696d934743920177
Giuliano Losa <giuliano@losa.fr>
2016-06-21 15:36:26 -0400

    Completed the proof of the soundness of history variable constructions.

--- History.thy
+++ History.thy
@@ -1,17 +1,21 @@
 theory History
-imports Simulations
+imports Simulations IOA_Automation
 begin
          
 context IOA begin
 
+context begin
+
 definition ioa_unfolding :: "('s,'a)ioa \<Rightarrow> (('s,'a)execution,'a)ioa" where
   "ioa_unfolding a \<equiv> let
       start = {(s,[]) | s . s \<in> start a};
-      trans = {(e,act,e') . (last_state e) \<midarrow>act\<midarrow>a\<longrightarrow> (last_state e') \<and> (fst o hd o snd) e' = act}
+      trans = {(e,act,e') . (last_state e) \<midarrow>act\<midarrow>a\<longrightarrow> (last_state e') 
+        \<and> e' = cons_exec e (act, last_state e')}
     in
       \<lparr>ioa.asig = ioa.asig a, ioa.start = start, ioa.trans = trans\<rparr>"
 
-lemma "is_ref_map last_state (ioa_unfolding a) a"
+private 
+lemma l1:"is_ref_map last_state (ioa_unfolding a) a"
 proof -
   { fix e e' act
     assume "reachable (ioa_unfolding a) e" and tr:"e \<midarrow>act\<midarrow>(ioa_unfolding a) \<longrightarrow>e'"
@@ -22,10 +26,12 @@ proof -
   thus ?thesis by (auto simp add:is_ref_map_def, simp add:ioa_unfolding_def, blast)
 qed
 
+private
 definition hist_fwd_sim :: "('s,'a)ioa \<Rightarrow> 's \<Rightarrow> (('s,'a)execution set)" where    
   "hist_fwd_sim a s \<equiv> {e . is_exec_of a e \<and> last_state e = s}"
-                                
-lemma "is_forward_sim (hist_fwd_sim a) a (ioa_unfolding a)"
+
+private                              
+lemma l2:"is_forward_sim (hist_fwd_sim a) a (ioa_unfolding a)"
 proof -
   text {* The base case *}
   { fix s assume "s \<in> start a"
@@ -52,15 +58,83 @@ proof -
   ultimately show ?thesis by (smt IOA.is_forward_sim_def)
 qed
 
+private
+lemma traces_ioa_unfolding:"traces (ioa_unfolding a) = traces a" using l1 l2 ref_map_soundness forward_sim_soundness
+by (metis (no_types, lifting) ioa.select_convs(1) ioa_unfolding_def set_eq_subset)
+
 definition add_history :: "('s,'a)ioa \<Rightarrow> ('s \<Rightarrow> 'h \<Rightarrow> 'a \<Rightarrow> 's \<Rightarrow> 'h) \<Rightarrow> ('s \<Rightarrow> 'h) \<Rightarrow> ('s\<times>'h,'a)ioa"
   where "add_history a f f\<^sub>0 \<equiv> let
       start = {(s,h) . s \<in> start a \<and> h = f\<^sub>0 s};
-      trans = {((s,h),act,(s',h')) . (s,act,s') \<in> trans a \<and> h' = f s h act s'}
+      trans = {((s,h),act,(s',h')) .  s \<midarrow>act\<midarrow>a\<longrightarrow> s' \<and> h' = f s h act s'}
     in
       \<lparr>ioa.asig = ioa.asig a, ioa.start = start, ioa.trans = trans\<rparr>"
 
-theorem "IOA.traces (add_history a f f\<^sub>0) = IOA.traces a"
-oops
+private
+definition hist_ref_map :: "('s \<Rightarrow> 'h \<Rightarrow> 'a \<Rightarrow> 's \<Rightarrow> 'h) \<Rightarrow> ('s \<Rightarrow> 'h) \<Rightarrow> ('s,'a)execution \<Rightarrow> 's\<times>'h" where 
+  "hist_ref_map f f\<^sub>0 e \<equiv> foldr (\<lambda> (a, s') (s, h) . (s', f s h a s'))
+    (snd e) (fst e, f\<^sub>0 (fst e))"
+
+private 
+lemma l5:"fst (hist_ref_map f f\<^sub>0 e) = last_state e"
+proof (induct e rule:last_state.induct)
+  case 1 thus ?case by (auto simp add:hist_ref_map_def)
+next 
+  case (2 s p ps)
+  thus ?case apply (auto simp add:hist_ref_map_def) by (metis (no_types, lifting) fstI prod.case_eq_if)
+qed
+
+private
+lemma l6:
+  assumes "e' = cons_exec e (a,s)"
+  shows "let hs = \<lambda> e . hist_ref_map f f\<^sub>0 e in hs e' = (s, f (last_state e) (snd (hs e)) a s)" 
+nitpick[verbose,  card 'a = 2, card 'b = 2, card 'c = 2, card "('b \<times> 'a) list" = 30, card "'a \<times> 'c" = 4, expect=none]
+using assms
+proof (induct "snd e")
+  case Nil thus ?case by (simp add:hist_ref_map_def cons_exec_def, cases e, cases e', auto)
+next
+  case (Cons x xs) thus ?case by (simp add:hist_ref_map_def cons_exec_def, cases e, cases e', auto)
+    (metis (no_types, lifting) case_prod_beta fstI)
+qed
+
+private
+lemma l4:"is_ref_map (hist_ref_map f f\<^sub>0) (ioa_unfolding a) (add_history a f f\<^sub>0)"
+apply (auto simp only:is_ref_map_def)
+    apply(simp add: ioa_unfolding_def hist_ref_map_def add_history_def)
+    (* base case discharged *)
+  subgoal premises prems for fst_e snd_e fst_e' snd_e' act
+  proof -
+    let ?e = "(hist_ref_map f f\<^sub>0 (fst_e, snd_e), [(act, hist_ref_map f f\<^sub>0 (fst_e', snd_e'))])"
+    show ?thesis using prems
+    apply (intro exI[where ?x="?e"])
+    apply (auto simp add:refines_def)
+        defer
+        apply (simp add:hist_ref_map_def is_trans_def trace_match_def ioa_unfolding_def add_history_def Let_def traces_simps)
+      apply (rm_reachable)
+      subgoal premises prems
+      proof -
+        from prems have 1:"(fst_e', snd_e') = cons_exec (fst_e, snd_e) (act, last_state (fst_e', snd_e'))" 
+        and 2:"last_state (fst_e, snd_e) \<midarrow>act\<midarrow>a\<longrightarrow> last_state (fst_e', snd_e')"
+          by (simp_all add:ioa_unfolding_def is_trans_def)
+        from 1 have 3:"fst_e = fst_e'" by (simp add:cons_exec_def)
+        from 1 2 3 l6[of "(fst_e', snd_e')" "(fst_e, snd_e)" act "last_state (fst_e', snd_e')" f f\<^sub>0] show ?thesis 
+        apply (simp add:add_history_def is_trans_def Let_def, cases "hist_ref_map f f\<^sub>0 (fst_e', snd_e)", auto)
+            apply (metis fst_conv l5)
+          apply (metis fst_conv l5) 
+        done
+      qed
+    done
+  qed
+done
+
+private
+lemma l3:"traces (ioa_unfolding a) \<subseteq> IOA.traces (add_history a f f\<^sub>0)" 
+using l4 ref_map_soundness
+by (metis (no_types, lifting) add_history_def ioa.select_convs(1) ioa_unfolding_def) 
+
+theorem "IOA.traces a \<subseteq> IOA.traces (add_history a f f\<^sub>0)"
+using traces_ioa_unfolding l3 by fast
+
+end
 
 end
 
